a1443a6b6fd89baacdd34ed5868b8efbcacc0ee5
Sandeep Kumar <166508938+SandeepKumarSolanki@users.noreply.github.com>
2026-01-06 23:02:06 +0530

    get Api

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ export const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
     res.json({message: "Api is working"})
 })
 app.use("/api", healthRoutes);
